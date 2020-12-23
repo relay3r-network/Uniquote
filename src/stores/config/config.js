259b60807b670349ff1cb86e0c090ef2aa55fd23
Akshay CM <akshaycm@hotmail.com>
2020-12-23 21:25:37 +0300

    Update oracle address to new oracle

--- src/stores/config/config.js
+++ src/stores/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   provider: 'https://mainnet.infura.io/v3/211c5f35ed97405b86465c1d76502922',
   // unioracleAddress: '0x127a2975c4E1c75f1ed4757a861bbd42523DB035',
-  keep3rOracleAddress: '0x8CCAE8392D251d4AcCb2174108A94B4847f17780',
+  keep3rOracleAddress: '0x314BefDD09623AeDb1774d3DDEAABbf5C4D57d01',
   keep3rVolatilityAddress: '0x14d551c19E6F2F2828b77AF62e7b34a9e07b0672'
 };
 
